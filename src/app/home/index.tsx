a777a5aac834bb94e7eb2b09e63f1fac13d5862d
laryssasouzas <laarylaryy@gmail.com>
2024-09-12 16:42:58 -0300

    alterar Section para renderizar contacts

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -3,7 +3,7 @@ import { Feather } from '@expo/vector-icons'
 import { theme } from "@/theme/index"
 import { styles } from './styles'
 import { Input } from '@/app/components/input'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useId } from 'react'
 import { Contact, ContactProps } from "@/app/components/contact"
 import * as Contacts from 'expo-contacts'
 
@@ -21,7 +21,26 @@ async function fetchContacts() {
         const { status } = await Contacts.requestPermissionsAsync()
         if (status === Contacts.PermissionStatus.GRANTED){
             const { data } = await Contacts.getContactsAsync()
-            console.log(data)
+            const list = data.map((contact) => ({
+                id: contact.id ?? useId(),
+                name: contact.name,
+                image: contact.image,
+            
+        
+        })).reduce<SectionListDataProps[]>((acc: any, item) => {
+            const firstletter = item.name[0].toUpperCase()
+            const existingEntry = acc.find((entry: SectionListDataProps) => 
+            (entry.title === firstletter))
+
+            if(existingEntry){
+                existingEntry.data.push(item)
+            } else {
+                acc.push({title: firstletter, data: [item]})
+            }
+            return acc
+        }, [])
+        setContacts(list)
+
         }
     } catch(error){
         console.log(error)
@@ -49,14 +68,11 @@ async function fetchContacts() {
                 sections={[{title: "R", data: [{id: "1", name: "Heloísa"}] }]}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
-                    <Contact contact={{
-                        name: item.name,
-                        image: require("@/assets/avatar.jpeg")
-                        }} 
-                    />   
+                    <Contact contact={item} />   
                 )}
-                renderSectionHeader = {({ section }) => 
-                    (<Text style={styles.section}>{section.title}</Text>)}
+                renderSectionHeader = {({ section }) => (
+                    <Text style={styles.section}>{section.title}</Text>
+                )}
                 contentContainerStyle = {styles.contentList}
                 />     
         </View>
